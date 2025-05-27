a6b2982b45f71a3a0ad3f186a5ac388c6a8dbdfc
Santech <scdiplomacy@gmail.com>
2025-05-26 23:01:03 -0300

    Ultimo Commit Pre entrega

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,8 +1,25 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_httpauth import HTTPBasicAuth
 import secrets
+from flasgger import Swagger
+
+# Configuración de Swagger con definiciones de seguridad
+template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "API de Ejemplo",
+        "description": "Esta API implementa autenticación básica utilizando Flask y Flasgger.",
+        "version": "1.0"
+    },
+    "securityDefinitions": {
+        "basicAuth": {
+            "type": "basic"
+        }
+    }
+}
 
 app = Flask(__name__)
+swagger = Swagger(app, template=template)
 auth = HTTPBasicAuth()
 
 # Diccionario de usuarios de ejemplo
@@ -19,6 +36,20 @@ def verify_password(username, password):
 @app.route('/api/protegida', methods=['GET'])
 @auth.login_required
 def api_protegida():
+    """
+    Endpoint protegido que requiere autenticación básica.
+    ---
+    security:
+      - basicAuth: []
+    responses:
+      200:
+        description: Acceso concedido exitosamente.
+        examples:
+          application/json:
+            mensaje: "Acceso concedido a admin!"
+      401:
+        description: Credenciales inválidas.
+    """
     return jsonify({"mensaje": f"Acceso concedido a {auth.current_user()}!"})
 
 if __name__ == '__main__':
